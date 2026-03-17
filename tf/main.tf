37de8b62ddc397aee4bf69dd46ce70198129f0cc
Arthur Morel-Réauté <arthur@platy.bzh>
2026-03-17 10:05:32 +0100

    chore: better resource naming

--- tf/main.tf
+++ tf/main.tf
@@ -7,26 +7,25 @@ terraform {
       version = "~> 4.64"
     }
   }
-
 }
 
 provider "azurerm" {
   features {}
 }
 
-resource "azurerm_resource_group" "example" {
-  name     = "example-resources"
+resource "azurerm_resource_group" "sup_de_vinci_hackathon" {
+  name     = "sup-de-vinci-hackathon-resources"
   location = "polandcentral"
 }
 
-resource "azurerm_kubernetes_cluster" "example" {
-  name                = "aks-qsdqzdsdsdgi"
-  location            = azurerm_resource_group.example.location
-  resource_group_name = azurerm_resource_group.example.name
-  dns_prefix          = "aks-qsdqzdsdsdgi"
+resource "azurerm_kubernetes_cluster" "devops03_aks" {
+  name                = "devops03-aks-cluster"
+  location            = azurerm_resource_group.sup_de_vinci_hackathon.location
+  resource_group_name = azurerm_resource_group.sup_de_vinci_hackathon.name
+  dns_prefix          = "devops03-aks"
 
   default_node_pool {
-    name       = "default"
+    name       = "defaultpool"
     node_count = 1
     vm_size    = "Standard_D2s_v3"
     upgrade_settings {
@@ -42,56 +41,56 @@ resource "azurerm_kubernetes_cluster" "example" {
 }
 
 output "kube_config" {
-  value     = azurerm_kubernetes_cluster.example.kube_config_raw
+  value     = azurerm_kubernetes_cluster.devops03_aks.kube_config_raw
   sensitive = true
 }
 
-resource "azurerm_container_registry" "example" {
-  name                = "acrqsdqzdsdsdgi"
-  resource_group_name = azurerm_resource_group.example.name
-  location            = azurerm_resource_group.example.location
+resource "azurerm_container_registry" "devops03_acr" {
+  name                = "devops03acr"
+  resource_group_name = azurerm_resource_group.sup_de_vinci_hackathon.name
+  location            = azurerm_resource_group.sup_de_vinci_hackathon.location
   sku                 = "Basic"
 
   admin_enabled = true
 }
 
 resource "azurerm_role_assignment" "acr_pull" {
-  principal_id         = azurerm_kubernetes_cluster.example.identity[0].principal_id
+  principal_id         = azurerm_kubernetes_cluster.devops03_aks.identity[0].principal_id
   role_definition_name = "AcrPull"
-  scope                = azurerm_container_registry.example.id
+  scope                = azurerm_container_registry.devops03_acr.id
 }
 
-resource "azurerm_log_analytics_workspace" "example" {
-  name                = "examplelaw"
-  location            = azurerm_resource_group.example.location
-  resource_group_name = azurerm_resource_group.example.name
+resource "azurerm_log_analytics_workspace" "sup_de_vinci_law" {
+  name                = "sup-de-vinci-law"
+  location            = azurerm_resource_group.sup_de_vinci_hackathon.location
+  resource_group_name = azurerm_resource_group.sup_de_vinci_hackathon.name
   sku                 = "PerGB2018"
   retention_in_days   = 30
 }
 
-resource "azurerm_application_insights" "example" {
-  name                = "example-app-insights"
-  location            = azurerm_resource_group.example.location
-  resource_group_name = azurerm_resource_group.example.name
+resource "azurerm_application_insights" "devops03_app_insights" {
+  name                = "devops03-app-insights"
+  location            = azurerm_resource_group.sup_de_vinci_hackathon.location
+  resource_group_name = azurerm_resource_group.sup_de_vinci_hackathon.name
   application_type    = "web"
-  workspace_id        = azurerm_log_analytics_workspace.example.id
+  workspace_id        = azurerm_log_analytics_workspace.sup_de_vinci_law.id
 }
 
 resource "azurerm_role_assignment" "log_analytics" {
-  principal_id         = azurerm_kubernetes_cluster.example.identity[0].principal_id
+  principal_id         = azurerm_kubernetes_cluster.devops03_aks.identity[0].principal_id
   role_definition_name = "Log Analytics Reader"
-  scope                = azurerm_monitor_workspace.example.id
+  scope                = azurerm_log_analytics_workspace.sup_de_vinci_law.id
 }
 
-resource "azurerm_monitor_workspace" "example" {
-  name                = "example-mamw"
-  resource_group_name = azurerm_resource_group.example.name
-  location            = azurerm_resource_group.example.location
+resource "azurerm_monitor_workspace" "sup_de_vinci_monitor" {
+  name                = "sup-de-vinci-monitor"
+  resource_group_name = azurerm_resource_group.sup_de_vinci_hackathon.name
+  location            = azurerm_resource_group.sup_de_vinci_hackathon.location
 }
 
-resource "azurerm_dashboard_grafana" "example" {
-  name                              = "example-dg"
-  resource_group_name               = azurerm_resource_group.example.name
+resource "azurerm_dashboard_grafana" "devops03_grafana" {
+  name                              = "devops03-grafana"
+  resource_group_name               = azurerm_resource_group.sup_de_vinci_hackathon.name
   location                          = "norwayeast"
   grafana_major_version             = 12
   api_key_enabled                   = true
@@ -101,8 +100,9 @@ resource "azurerm_dashboard_grafana" "example" {
   identity {
     type = "SystemAssigned"
   }
+
   azure_monitor_workspace_integrations {
-    resource_id = azurerm_monitor_workspace.example.id
+    resource_id = azurerm_monitor_workspace.sup_de_vinci_monitor.id
   }
 }
 
@@ -111,9 +111,9 @@ data "azurerm_client_config" "current_user" {}
 resource "azurerm_role_assignment" "grafana_viewer" {
   principal_id         = data.azurerm_client_config.current_user.object_id
   role_definition_name = "Grafana Viewer"
-  scope                = azurerm_dashboard_grafana.example.id
+  scope                = azurerm_dashboard_grafana.devops03_grafana.id
 }
 
 output "grafana_dashboard_url" {
-  value = azurerm_dashboard_grafana.example.endpoint
+  value = azurerm_dashboard_grafana.devops03_grafana.endpoint
 }
